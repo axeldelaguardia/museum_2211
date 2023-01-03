0a245282fe250c3b6d8f12612bc5b8715676c0ee
Axel De La Guardia <axeldelaguardia@gmail.com>
2023-01-03 09:59:50 -0700

    Fix: sorting of array for #recommend_exhibit test

--- spec/museum_spec.rb
+++ spec/museum_spec.rb
@@ -4,7 +4,7 @@ describe Museum do
 	let(:dmns) {Museum.new("Denver Museum of Nature and Science")}
 
 	let(:gems_and_minerals) {Exhibit.new({name: "Gems and Minerals", cost: 0})}
-	let(:dead_sea_scroll) {Exhibit.new({name: "Dead Sea Scroll", cost: 10})}
+	let(:dead_sea_scroll) {Exhibit.new({name: "Dead Sea Scrolls", cost: 10})}
 	let(:imax) {Exhibit.new({name: "IMAX", cost: 15})}
 
 	let(:patron_1) {Patron.new("Bob", 20)}
@@ -40,8 +40,14 @@ describe Museum do
 	end
 
 	describe '#recommend_exhibits' do
+		before do
+			dmns.add_exhibit(gems_and_minerals)
+			dmns.add_exhibit(dead_sea_scroll)
+			dmns.add_exhibit(imax)
+		end
+
 		it 'can recommend exhibits to patrons that match patrons interests' do
-			expect(dmns.recommend_exhibits(patron_1)).to eq([dead_sea_scroll, gems_and_minerals])
+			expect(dmns.recommend_exhibits(patron_1)).to eq([gems_and_minerals, dead_sea_scroll])
 			expect(dmns.recommend_exhibits(patron_2)).to eq([imax])
 		end
 	end
