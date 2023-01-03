84be72690409394b83bcbb99d47c529b3335357b
Axel De La Guardia <axeldelaguardia@gmail.com>
2023-01-03 09:19:47 -0700

    Add: #add_interests test

--- spec/patron_spec.rb
+++ spec/patron_spec.rb
@@ -1,17 +1,26 @@
 require './spec/spec_helper'
 
 describe Patron do
-	let(:patron) {Patron.new("Bob", 20)}
+	let(:patron_1) {Patron.new("Bob", 20)}
 
 	describe '#initialize' do
 		it 'exists' do
-			expect(patron).to be_a(Patron)
+			expect(patron_1).to be_a(Patron)
 		end
 
 		it 'has attributes' do
-			expect(patron.name).to eq("Bob")
-			expect(patron.spending_money).to eq(20)
-			expect(patron.interests).to eq([])
+			expect(patron_1.name).to eq("Bob")
+			expect(patron_1.spending_money).to eq(20)
+			expect(patron_1.interests).to eq([])
+		end
+	end
+
+	describe '#add_interests' do
+		it 'can add interests to an array' do
+			patron_1.add_interests("Dead Sea Scrolls")
+			patron_1.add_interests("Gems and Minerals")
+
+			expect(patron_1.interests).to eq(["Dead Sea Scrolls", "Gems and Minerals"])
 		end
 	end
 end
