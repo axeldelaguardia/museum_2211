15ba0683731c511d19a174463e4de22d964dcc3c
Axel De La Guardia <axeldelaguardia@gmail.com>
2023-01-03 09:35:53 -0700

    Add: setup and test for #recommend_exhibits

--- spec/museum_spec.rb
+++ spec/museum_spec.rb
@@ -2,9 +2,19 @@ require './spec/spec_helper'
 
 describe Museum do
 	let(:dmns) {Museum.new("Denver Museum of Nature and Science")}
+
 	let(:gems_and_minerals) {Exhibit.new({name: "Gems and Minerals", cost: 0})}
 	let(:dead_sea_scroll) {Exhibit.new({name: "Dead Sea Scroll", cost: 10})}
 	let(:imax) {Exhibit.new({name: "IMAX", cost: 15})}
+
+	let(:patron_1) {Patron.new("Bob", 20)}
+	let(:patron_2) {Patron.new("Sally", 20)}
+
+	before do
+		patron_1.add_interests("Dead Sea Scrolls")
+		patron_1.add_interests("Gems and Minerals")
+		patron_2.add_interests("IMAX")
+	end
 
 	describe '#initialize' do
 		it 'exists' do
@@ -26,6 +36,13 @@ describe Museum do
 
 		it "can add exhibits to its array" do
 			expect(dmns.exhibits).to eq([gems_and_minerals, dead_sea_scroll, imax])
+		end
+	end
+
+	describe '#recommend_exhibits' do
+		it 'can recommend exhibits to patrons that match patrons interests' do
+			expect(dmns.recommend_exhibits(patron_1)).to eq([dead_sea_scroll, gems_and_minerals])
+			expect(dmns.recommend_exhibits(patron_2)).to eq([imax])
 		end
 	end
 end
